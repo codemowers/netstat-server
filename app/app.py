d095e3f53b1630ea45cda38c0739de0f1176ac05
Lauri Võsandi <lauri@k-space.ee>
2023-04-12 09:23:31 +0300

    Handle situation where process has no TCP/IP stack at all

--- app/app.py
+++ app/app.py
@@ -60,25 +60,29 @@ async def export(request):
         cid, = m.groups()
         cid = "cri-o://%s" % cid
 
-        with open(os.path.join(PATH_PROCFS, "%d/net/tcp" % pid), "r") as fh:
-            fh.readline()
-            for line in fh:
-                cells = re.split(r"\s+", line.strip())
-                laddr, lport = cells[1].split(":")
-                raddr, rport = cells[2].split(":")
-                state = STATES[int(cells[3], 16)]
-                lport, rport = int(lport, 16), int(rport, 16)
-                laddr = socket.inet_ntoa(struct.pack("<L", int(laddr, 16)))
-                raddr = socket.inet_ntoa(struct.pack("<L", int(raddr, 16)))
+        try:
+            with open(os.path.join(PATH_PROCFS, "%d/net/tcp" % pid), "r") as fh:
+                fh.readline()
+                for line in fh:
+                    cells = re.split(r"\s+", line.strip())
+                    laddr, lport = cells[1].split(":")
+                    raddr, rport = cells[2].split(":")
+                    state = STATES[int(cells[3], 16)]
+                    lport, rport = int(lport, 16), int(rport, 16)
+                    laddr = socket.inet_ntoa(struct.pack("<L", int(laddr, 16)))
+                    raddr = socket.inet_ntoa(struct.pack("<L", int(raddr, 16)))
 
-                if laddr.startswith("127."):
-                    continue
-                if state == "LISTEN":
-                    assert raddr == "0.0.0.0"
-                    z["listening"].append((cid, lport, "TCP"))
-                    continue
+                    if laddr.startswith("127."):
+                        continue
+                    if state == "LISTEN":
+                        assert raddr == "0.0.0.0"
+                        z["listening"].append((cid, lport, "TCP"))
+                        continue
 
-                z["connections"].append((cid, lport, raddr, rport, "TCP", state, reverse_lookup.get(raddr)))
+                    z["connections"].append((cid, lport, raddr, rport, "TCP", state, reverse_lookup.get(raddr)))
+        except FileNotFoundError:
+            # TODO: no network stack at all?
+            continue
     return json(z)
 
 
